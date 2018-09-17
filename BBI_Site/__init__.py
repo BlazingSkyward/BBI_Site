7c231ab6ac7ac6099c53471114bf7d67470cab31
Malik Abu-Kalokoh <malik.abukalokoh@yahoo.com>
2018-09-16 20:25:32 -0400

    init

---
+++ BBI_Site/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.config.from_envvar('PYTHONAPP_SETTINGS', silent=True) #Since silent is true then nothing will occur if mess up
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+from BBI_Site import routes, models
+db.create_all()
---
+++ BBI_Site/config.py
@@ -0,0 +1,12 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DATABASE = 'sqlite:////tmp/BBI_Site.db' #tmp folder is deleted nightly
+    DEBUG = True
+    SECRET_KEY = "\xd2\xe2\xa8\xdf\xb2\x11\xe7\xd6\x86\xaf\x88"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'BBI_Site.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
+    TEMPLATES_AUTO_RELOAD = True
